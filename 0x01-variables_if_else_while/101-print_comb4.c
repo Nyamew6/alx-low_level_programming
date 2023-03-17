5da7abefe2bff528371358e96ea68e4aa7c32b3d
Nyamew6 <makorinyamweya9@gmail.com>
2023-03-17 15:28:29 -0700

    jhshjsd

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,33 +8,28 @@
  */
 int main(void)
 {
-    int i, j, k;
+	int i, j, k;
 
-    for (i = 0; i <= 7; i++)
-    {
-        for (j = i + 1; j <= 8; j++)
-        {
-            for (k = j + 1; k <= 9; k++)
-            {
-                if ((i == 0 && j == 1 && k == 2) || (i == 1 && j == 0 && k == 2) ||
-                    (i == 0 && j == 2 && k == 1) || (i == 1 && j == 2 && k == 0) ||
-                    (i == 2 && j == 0 && k == 1) || (i == 2 && j == 1 && k == 0))
-                    continue;
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				putchar(i % 10 + '0');
+				putchar(j % 10 + '0');
+				putchar(k % 10 + '0');
 
-                putchar(i % 10 + '0');
-                putchar(j % 10 + '0');
-                putchar(k % 10 + '0');
+				if (i == 7 && j == 8 && k == 9)
+					continue;
 
-                if (i == 7 && j == 8 && k == 9)
-                    continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+	putchar('\n');
 
-    putchar('\n');
-
-    return (0);
+	return (0);
 }
