57282dd8391309151357ab44f9b030db08ae19ab
Nyamew6 <makorinyamweya9@gmail.com>
2023-03-22 09:24:33 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,89 +1,29 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * fibonacci_sum_even - Computes sum of even Fibonacci numbers
- * that do not exceed 4 million
+ * main - Prints the add of the even-valued
+ * fibonacci numbers.
  *
- * Return: Sum of even Fibonacci numbers
+ * Return: Always 0.
  */
-int fibonacci_sum_even(void)
+int main(void)
 {
-	int i, sum = 0, fib;
+	long int n1, n2, fn, afn;
 
-	for (i = 0; (fib = fibonacci(i)) <= 4000000; i++)
+	n1 = 1;
+	n2 = 2;
+	fn = afn = 0;
+	while (fn <= 4000000)
 	{
-		if (fib % 2 == 0)
-			sum += fib;
+		fn = n1 + n2;
+		n1 = n2;
+		n2 = fn;
+		if ((n1 % 2) == 0)
+		{
+			afn += n1;
+		}
 	}
-
-	return (sum);
-}
-
-/**
- * fibonacci - Computes the nth Fibonacci number
- * @n: The index of the Fibonacci number to compute
- *
- * Return: The nth Fibonacci number
- */
-int fibonacci(int n)
-{
-	int i, a = 1, b = 1, c;
-
-	if (n < 2)
-		return (1);
-
-	for (i = 2; i <= n; i++)
-	{
-		c = a + b;
-		a = b;
-		b = c;
-	}
-
-	return (b);
-}
-
-/**
- * num_len - Computes the number of digits in a number
- * @num: The number to compute the length of
- *
- * Return: The number of digits in @num
- */
-int num_len(int num)
-{
-	int len = 0;
-
-	do {
-		len++;
-		num /= 10;
-	} while (num != 0);
-
-	return (len);
-}
-
-/**
- * print_num - Prints a number to the standard output
- * @num: The number to print
- */
-void print_num(int num)
-{
-	int i, len, div = 1;
-
-	if (num < 0)
-	{
-		_putchar('-');
-		num = -num;
-	}
-
-	len = num_len(num);
-
-	for (i = 0; i < len - 1; i++)
-		div *= 10;
-
-	for (i = 0; i < len; i++)
-	{
-		_putchar('0' + num / div);
-		num %= div;
-		div /= 10;
-	}
+	printf("%ld\n", afn);
+	return (0);
 }
 
