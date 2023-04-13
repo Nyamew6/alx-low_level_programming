b4a1902f4227b5ac8d08c75ef520398bcf883dea
Nyamew6 <makorinyamweya9@gmail.com>
2023-04-13 13:31:58 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,23 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
+ * create_array - create array of size size and assign char c
+ * @size: size of array
+ * @c: char to assign
+ * Description: creat array of size size and assign char c
+ * Return: pointer to array, NULL if fail
  */
-
-int main(int argc __attribute__((unused)), char *argv[])
+char *create_array(unsigned int size, char c)
 {
-	printf("%s\n", *argv);
-	return (0);
+	char *str;
+	unsigned int i;
+
+	str = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
 }
